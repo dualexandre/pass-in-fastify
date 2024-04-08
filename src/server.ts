3f68247ca3426a1b8ad800f7211bdbb0d2f59931
Eduardo Alexandre <luis.edu.alex@gmail.com>
2024-04-08 15:13:43 -0300

    feat: add event and badge routes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import {
 import fastify from 'fastify';
 import { createEvent } from './routes/create-event';
 import { registerForEvent } from './routes/register-for-event';
+import { getEvent } from './routes/get-event';
+import { getAttendeeBadge } from './routes/get-attendee-badge';
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
@@ -18,6 +20,8 @@ app.get('/', () => {
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
